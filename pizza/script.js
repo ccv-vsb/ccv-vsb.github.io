efae4fb85b6f49d6ad421912a7a5d45270ad180b
ccv-vsb <75321198+ccv-vsb@users.noreply.github.com>
2021-05-26 15:21:45 +0200

    Add files via upload

--- pizza/script.js
+++ pizza/script.js
@@ -1,71 +1,81 @@
 $(document).ready(function () {
-    $("#objednavka-shrnuti").html("<em>Nemate nic vybrano</em>");
-    $("#pizza-shrnuti").html("<em>Zatim zadna pizza</em>");
-    var ingredientArray = document.getElementsByName("ingredience[]");
-    //console.log(ingredientArray);
-
-    var cena;
-    $(".pizza").click(function () {
-        $("#pizza-shrnuti").text(this.getAttribute("data-pizzaType"));
-        cena = parseInt(this.getAttribute("data-price"));
-        $("#zaPizzu").text(cena);
-        console.log(pocet);
-    });
-
+    var ingredientsArray = document.getElementsByName("ingredience[]");
+    console.log(ingredientsArray);
     var pocet = 1;
-    $("#pocet").change(function () {
-        pocet = parseInt(this.value);
-        $("#zaPizzu").text(pocet * cena);
-        console.log(pocet);
-        $("#pizza-pocet").html(pocet + " ks<br>");
-    });
-
-    var zaDoplnky;
-    $(".ingredience-navic").click(function () {
-        var pridat = "";
-        zaDoplnky = 0;
-        for (let index = 0; index < ingredientArray.length; index++) {
-            if (ingredientArray[index].checked) {
-                pridat += ingredientArray[index].getAttribute("data-value") + "<br>";
-                zaDoplnky += parseInt(ingredientArray[index].getAttribute("data-price"));
-            }
-            $("#pridano").html(pridat);
-            $("#zaPizzu").text(pocet * (cena + zaDoplnky));
-        }
-    });
-
-    var zaTutoPizzu;
     var celkem = 0;
-    $("#pridej").click(function () {
-        zaTutoPizzu = pocet * (cena + zaDoplnky);
-        $("#kosik").append("<div class='polozka' data-price=" + zaTutoPizzu + ">" + $("#pizza-shrnuti").html() + "<br>" + $("#pridano").html() + "<br>" + $("#pizza-pocet").html() + "<button class='odebrat'>Odebrat</button></div>");
-
-        for (let index = 0; index < ingredientArray.length; index++) {
-            ingredientArray[index].checked = false;
+    
+    var cena;
+    $('.pizza').click(function () {
+        $('#pizza-shrnuti').text(this.value);
+        cena = parseInt(this.getAttribute("data-price"));
+        $('#checkboxy').show();
+        console.log(cena);
+        aktualizujCenu();
+    });
+    $('#pocet').change(function () {
+        pocet = parseInt(this.value);
+        console.log(pocet);
+        $('#pizza-pocet').text(pocet);
+        aktualizujCenu();
+    });
+    
+    var zaDoplnky = 0;
+    $('.ingredience-navic').click(function () {
+        var pridatIngr = "";
+        zaDoplnky = 0;
+        for (let index = 0; index < ingredientsArray.length; index++) {
+            if (ingredientsArray[index].checked) {
+                pridatIngr += ingredientsArray[index].getAttribute("data-value") + "<br>";
+                zaDoplnky += parseInt(ingredientsArray[index].getAttribute("data-price"));
+            }
         }
-        celkem += zaTutoPizzu;
-        $("#zaPizzu").text(celkem);
-        zaDoplnky=0;
-        $("#pizza-shrnuti").text("Vyberte dalsi pizzu");
-        $("#pridano").text("");
-        $("#pizza-pocet").text("");
-        $("#pocet").val(1);
+        $('#pridano').html(pridatIngr);
+        aktualizujCenu();
     });
-
+    
+    $('#pridej').click(function () {
+        $('#kosik').append('<div class=".polozka" data-price=' + $("#cena-za-pizzu").text() + '>' + $('#pizza-shrnuti').html() + "<br>" + $('#pizza-pocet').html() + "<br>" + $("#pridano").html() + "<br>" + "<button class='odebrat'>Odebrat</button></div>");
+        celkem += pocet * (cena + zaDoplnky);
+        aktualizujCenu();
+        
+        vynuluj();
+    });
+    
     $(document).on('click', '.odebrat', function () {
-        $(this).closest('.polozka').remove();
-        //bud...
-        // cenaKodecteni = $(this).closest('.polozka').attr("data-price");
-        // celkem-=parseInt(cenaKodecteni);
-        //...anebo...
-        celkem -= parseInt($(this).closest('.polozka').attr("data-price"));
-        $("#zaPizzu").text(celkem);
+        celkem -= parseInt($(this).closest('div').attr("data-price"));
+        console.log(celkem);
+        $(this).closest('div').remove();
+        aktualizujCenu();
+    });
+    // $('.odebrat').click(function(){});
+    
+    $('#sladka').click(function () {
+        $('#checkboxy').hide();
     });
 
-    $("#odeslat").click(function () {
-        $('#odeslat').before('<p id="odeslani-zprava">Objednavka odeslana! </p>');
-        $(this).addClass("odeslano");
-        // });
+    $('#odeslat').click(function(){
+$('#odeslat').after('<p>Objednavka odeslana!</p>')
     });
+    
+    function aktualizujCenu() {
+        $('#cena-za-pizzu').text(pocet * (cena + zaDoplnky));
+        $('#cena-celkem').text(celkem);
+    }
+    
+    function vynuluj() {
+        for (let index = 0; index < ingredientsArray.length; index++) {
+            ingredientsArray[index].checked = false;
+        }
+        zaDoplnky = 0;
+        pridatIngr = "";
+        $('#pizza-shrnuti').text('Vyberte dalsi pizzu');
+        $('#cena-za-pizzu').text("0");
+        $("#pridano").text('');
+        $('#pizza-pocet').text(1);
+        $("#pocet").val(1);
+        pocet = 1;
+    }
+
 
 });
+
